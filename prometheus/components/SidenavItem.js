e7ef815509d823633f826ada4e19530d3e9f00b8
varrockbank <233495851+varrockbank@users.noreply.github.com>
2025-12-13 13:34:39 +0100

    p: add router
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # On branch prometheus
    # Changes to be committed:
    #	modified:   components/SidenavItem.js
    #	modified:   index.html
    #

--- prometheus/components/SidenavItem.js
+++ prometheus/components/SidenavItem.js
@@ -3,15 +3,15 @@ import { store } from '../store.js';
 export const SidenavItem = {
   props: ['id', 'label', 'icon'],
   template: `
-    <div
+    <router-link
+      :to="'/' + id"
       class="sidenav-item"
       :class="{ active: store.activeMenuItem === id }"
       :data-tooltip="label"
-      @click="store.activeMenuItem = id"
     >
       <svg class="sidenav-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" v-html="icon"></svg>
       <span class="sidenav-label">{{ label }}</span>
-    </div>
+    </router-link>
   `,
   setup() {
     return { store };
