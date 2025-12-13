e42f6b43275fe2b9469ebdf26e60eb5c96e169f5
varrockbank <233495851+varrockbank@users.noreply.github.com>
2025-12-13 19:46:10 +0100

    b: remove toggle on sidenav

--- prometheus/components/Sidenav.js
+++ prometheus/components/Sidenav.js
@@ -7,13 +7,6 @@ export const Sidenav = {
   template: `
     <div class="sidenav" :class="{ collapsed: store.sidenavCollapsed }">
       <div class="sidenav-menu">
-        <div class="sidenav-item" data-tooltip="Toggle" @click="actions.toggleSidenav">
-          <svg class="sidenav-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <rect width="18" height="18" x="3" y="3" rx="2"/>
-            <path d="M9 3v18"/>
-          </svg>
-          <span class="sidenav-label">Toggle</span>
-        </div>
         <SidenavItem
           v-for="item in menuItems"
           :key="item.id"
